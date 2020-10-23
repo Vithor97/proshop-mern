a1ec8b4dea146d9486c81138a7bc7596b386a9d0
vithor97 <vitorsantos27@hotmail.com>
2020-10-23 16:43:49 -0300

    implementacao do react-router-dom

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Container, Navbar, Nav } from 'react-bootstrap'
+import {LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">Pro Shop</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Pro Shop</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Carrinho</Nav.Link>
-                        <Nav.Link href="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>
+                        <Nav.Link as={Link} to='/cart'><i className="fas fa-shopping-cart"></i>Carrinho</Nav.Link>
+                        <Nav.Link as={Link} to="/login"><i className="fas fa-user"></i>Sign In</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
